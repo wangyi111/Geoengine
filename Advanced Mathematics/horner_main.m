f069529b06fdbb2efc0895ccf3db7e5cf4e181b2
wangyi111 <35645723+wangyi111@users.noreply.github.com>
2018-11-07 19:02:54 +0100

    Add files via upload

---
+++ Advanced Mathematics/horner_main.m
@@ -0,0 +1,22 @@
+
+%% main function
+% input an and x0
+disp('Please input n,an and x0:')
+n=input("the order of the ODE: n=");
+an=zeros(1,n+1);
+an=input("the coefficient vector of the ODE: an=");
+x0=input("guess one root: x0=");
+disp('*****************************');
+xx=zeros(1,n);
+bb=zeros(n,n+1);
+[bb,xx]=horner(an,x0);
+
+disp(['the roots are:',num2str(xx)]);
+disp('the horner scheme parameters ai,bi(bi=ai+b(i+1)*x0) are:');
+for i=1:n
+  disp(['x0=',num2str(xx(i))]);  
+  disp(['ai:',num2str(an)]);
+  disp(['bi:',num2str(bb(i,:))]);
+%  disp(newline);
+end
+disp(['the general solution is:','y=C1*e^',num2str(xx(1)),'+C2*e^',num2str(xx(2)),'+C3*e^',num2str(xx(3)),'+C4*e^',num2str(xx(4))])
