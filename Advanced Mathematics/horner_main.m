24472263c420cdce3026da9cbb2f840c53c8a7aa
wangyi111 <35645723+wangyi111@users.noreply.github.com>
2018-12-09 14:42:00 +0100

    Update horner_main.m

--- Advanced Mathematics/horner_main.m
+++ Advanced Mathematics/horner_main.m
@@ -1,10 +1,17 @@
 
-%% main function
+%% advanced mathematics lab1 main function
+% author: Yi Wang
+% last change: 09/12/2018
+
 % input an and x0
 disp('Please input n,an and x0:')
 n=input("the order of the ODE: n=");
 an=zeros(1,n+1);
 an=input("the coefficient vector of the ODE: an=");
+% check dimension
+if length(an)!=n+1
+  error('dimension incorrect!');
+end
 x0=input("guess one root: x0=");
 disp('*****************************');
 xx=zeros(1,n);
